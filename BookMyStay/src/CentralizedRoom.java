597296de524b0882d2a430c3db85614277f2adc8
Pottral <pottralkural79@gmail.com>
2026-03-24 10:21:29 +0530

    Update CentralizedRoom.java

--- BookMyStay/src/CentralizedRoom.java
+++ BookMyStay/src/CentralizedRoom.java
@@ -1,2 +1,28 @@
 public class CentralizedRoom {
+
+    public static void main(String[] args) {
+
+        System.out.println("=====================================");
+        System.out.println(" Book My Stay App ");
+        System.out.println(" Version: 3.1 ");
+        System.out.println("=====================================");
+
+        // Initialize centralized inventory
+        RoomInventory inventory = new RoomInventory();
+
+        // Display initial inventory
+        inventory.displayInventory();
+
+        // Simulate updates
+        System.out.println("\nUpdating Inventory...\n");
+
+        inventory.updateAvailability("Single Room", -1); // Booking
+        inventory.updateAvailability("Double Room", -2); // Booking
+        inventory.updateAvailability("Suite Room", 1);   // Cancellation
+
+        // Display updated inventory
+        inventory.displayInventory();
+
+        System.out.println("\nApplication execution completed.");
+    }
 }
